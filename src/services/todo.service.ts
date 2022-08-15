161a997f85971f2b31ae9b44319a8bb421fa4aa3
nerdchanii <nerdchanii@gmail.com>
2022-08-15 19:05:49 +0900

    Add; todoService

---
+++ src/services/todo.service.ts
@@ -0,0 +1,35 @@
+import { InterfaceTodoApi, Todo } from "../Apis/types";
+import { TodoContent } from "../Apis/types";
+import TodoApi from "../Apis/TodoApi";
+import { todoApis } from "../Apis/Apis";
+
+
+class TodoService {
+  private api: InterfaceTodoApi;
+
+  constructor(){ 
+    this.api = todoApis;
+  }
+
+  async getTodos(){
+    return await this.api.getTodos();
+  }
+
+  async getTodoById(id: string) {
+    return await this.api.getTodoById(id);
+  }
+
+  async createTodo(todo: TodoContent) {
+    return await this.api.createTodo(todo);
+  }
+
+  async updateTodo(todo:Todo): Promise<any> {
+    return await this.api.updateTodo(todo);
+  }
+
+  async deleteTodo(id: string): Promise<any> {
+    return await this.api.deleteTodo(id);
+  }
+}
+
+export default new TodoService();
